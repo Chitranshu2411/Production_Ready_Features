34292a874d5a224c2e65d05396b930d3a47e5653
Chitranshu <chitranshu2411@gmail.com>
2026-04-16 23:53:30 +0530

    implement ApiError class for standardized error responses
    implement ApiError class for standardized error responses

--- src/main/java/com/codingshuttle/prod_ready_features/prod_ready_features/advice/ApiError.java
+++ src/main/java/com/codingshuttle/prod_ready_features/prod_ready_features/advice/ApiError.java
@@ -1,30 +1,52 @@
 package com.codingshuttle.prod_ready_features.prod_ready_features.advice;
 
-import lombok.Data;
 import org.springframework.http.HttpStatus;
-
 import java.time.LocalDateTime;
 
-@Data
 public class ApiError {
+
     private LocalDateTime timeStamp;
     private String error;
     private HttpStatus statusCode;
-
 
     public ApiError() {
         this.timeStamp = LocalDateTime.now();
     }
 
     public ApiError(HttpStatus statusCode, LocalDateTime timeStamp) {
-        this();
-        this.statusCode = statusCode;
         this.timeStamp = timeStamp;
+        this.statusCode = statusCode;
     }
 
     public ApiError(String localizedMessage, HttpStatus httpStatus) {
         this.error = localizedMessage;
         this.statusCode = httpStatus;
         this.timeStamp = LocalDateTime.now();
+    }
+
+    // getters
+    public LocalDateTime getTimeStamp() {
+        return timeStamp;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public HttpStatus getStatusCode() {
+        return statusCode;
+    }
+
+    // setters
+    public void setTimeStamp(LocalDateTime timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public void setStatusCode(HttpStatus statusCode) {
+        this.statusCode = statusCode;
     }
 }
